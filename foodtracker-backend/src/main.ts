2af523a0586a1344bc08853177b2c9f0ace09fa6
Ryan Anderson <sonorousduck@gmail.com>
2025-09-08 08:34:55 -0600

    Fixed a bunch of issues to run on windows

--- foodtracker-backend/src/main.ts
+++ foodtracker-backend/src/main.ts
@@ -2,15 +2,19 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ["http://localhost:8081", "http://10.0.0.83:8081"],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: [
+      'http://localhost:8081',
+      'http://10.0.0.83:8081',
+      'http://192.168.1.16:8081',
+      '*',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true, // If you need to send cookies
   });
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
