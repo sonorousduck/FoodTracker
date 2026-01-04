8dbd21a52e14c20e291ed9630081c397481e7a9f
Ryan Anderson <sonorousduck@gmail.com>
2026-01-04 07:50:42 -0700

    Updated tests so they don't fail

--- foodtracker-backend/src/food/food.service.spec.ts
+++ foodtracker-backend/src/food/food.service.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { FoodService } from './food.service';
+import { Food } from './entities/food.entity';
 
 describe('FoodService', () => {
   let service: FoodService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [FoodService],
+      providers: [
+        FoodService,
+        { provide: getRepositoryToken(Food), useValue: {} },
+      ],
     }).compile();
 
     service = module.get<FoodService>(FoodService);
--- foodtracker-backend/src/weight/weight.service.spec.ts
+++ foodtracker-backend/src/weight/weight.service.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { WeightService } from './weight.service';
+import { Weight } from './entities/weight.entity';
 
 describe('WeightService', () => {
   let service: WeightService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [WeightService],
+      providers: [
+        WeightService,
+        { provide: getRepositoryToken(Weight), useValue: {} },
+      ],
     }).compile();
 
     service = module.get<WeightService>(WeightService);
