d05ee9d7d971c085b8ec93d78932be4ab2d51fad
Ryan Anderson <sonorousduck@gmail.com>
2026-05-11 06:48:34 -0600

    Add title

--- foodtracker-frontend/app/+html.tsx
+++ foodtracker-frontend/app/+html.tsx
@@ -8,6 +8,7 @@ export default function Root({ children }: PropsWithChildren) {
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <title>Duck log</title>
 
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
